0289350d2f81c93749df16b3f510f0336dd733fa
Mayank Agrawal <47361818+amayank-umich@users.noreply.github.com>
2020-08-31 17:54:00 -0500

    Update Quest_jobscript_CPU.sh

--- Quest_jobscript_CPU.sh
+++ Quest_jobscript_CPU.sh
@@ -3,7 +3,7 @@
 #SBATCH -p normal               # Queue: short, normal, long
 #SBATCH -t 11:00:00             # Walltime/duration of the job
 #SBATCH -nodes 1                # Number of Nodes
-#SBATCH --ntasks-per-node=6     # Number of Cores (Processors)
+#SBATCH --ntasks-per-node=2     # Number of Cores (Processors)
 #SBATCH --mail-user=<email_id>  # Designate email address for job communications
 #SBATCH --mail-type=END         # Events options are job BEGIN, END, NONE, FAIL, REQUEUE
 #SBATCH --output=<file_path>    # Path for output must already exist - e.g. /path/job.out
@@ -25,7 +25,7 @@ module load gromacs/5.0.4
 # gmx grompp -f sim_eq.mdp -p PA.top -c sim_min.gro -o sim_eq.tpr -maxwarn 2
 
 ## Run mdrun on single core. Throw the output to out.log
-gmx mdrun -deffnm sim_eq -v -cpt 30 &> out.log
+# gmx mdrun -deffnm sim_eq -v -cpt 30 &> out.log
 
 ## Run mdrun on multiple cores. Throw the output to out.log
-# mpirun -np 2 gmx_mpi mdrun -deffnm sim_eq -v -cpt 30 &> out.log
+mpirun -np 2 gmx_mpi mdrun -deffnm sim_eq -v -cpt 30 &> out.log
