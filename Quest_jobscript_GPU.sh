fe5451915332c51038c0b7004a77e6f31b18fe06
Mayank Agrawal <47361818+amayank-umich@users.noreply.github.com>
2020-10-21 15:32:44 -0500

    Update Quest_jobscript_GPU.sh
    updated

--- Quest_jobscript_GPU.sh
+++ Quest_jobscript_GPU.sh
@@ -2,9 +2,10 @@
 #SBATCH -A p30009               # Allocation
 #SBATCH -p gengpu               # Queue: short, normal, long
 #SBATCH -t 11:00:00             # Walltime/duration of the job
-#SBATCH -nodes 1                # Number of Nodes
+#SBATCH -N 1                    # Number of Nodes
 #SBATCH --ntasks-per-node=2     # Number of Cores (Processors)
 #SBATCH --gres gpu:2            # Extra line for gpu jobs specifying number of gpus
+#SBATCH --mem=10G               # Memory request per node
 #SBATCH --mail-user=<email_id>  # Designate email address for job communications
 #SBATCH --mail-type=END         # Events options are job BEGIN, END, NONE, FAIL, REQUEUE
 #SBATCH --output=<file_path>    # Path for output must already exist - e.g. /path/job.out
@@ -23,7 +24,7 @@ module load gromacs/5.0.4-cuda
 ## A command you actually want to execute: Example showed for running gmx
 
 ## You may want to create the tpr for mdrun in the cluster core itself for compatibility reasons
-# gmx_mpi grompp -f sim_eq.mdp -p PA.top -c sim_min.gro -o sim_eq.tpr -maxwarn 2
+gmx_mpi grompp -f sim_eq.mdp -p PA.top -c sim_min.gro -o sim_eq.tpr -maxwarn 2
 
 ## Run mdrun on single core. Throw the output to out.log
 # mpirun -np 1 gmx_mpi mdrun -deffnm sim_eq -v -cpt 30 &> out.log
