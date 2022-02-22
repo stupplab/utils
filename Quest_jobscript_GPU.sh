055185addf21d2ec80e04840d35741fdc86e4cce
Mayank Agrawal <47361818+amayank-umich@users.noreply.github.com>
2022-02-22 17:52:22 -0600

    Update Quest_jobscript_GPU.sh
    Using lastest gmx version

--- Quest_jobscript_GPU.sh
+++ Quest_jobscript_GPU.sh
@@ -3,8 +3,8 @@
 #SBATCH -p gengpu               # Queue: short, normal, long
 #SBATCH -t 11:00:00             # Walltime/duration of the job
 #SBATCH -N 1                    # Number of Nodes
-#SBATCH --ntasks-per-node=2     # Number of Cores (Processors)
-#SBATCH --gres gpu:2            # Extra line for gpu jobs specifying number of gpus
+#SBATCH --ntasks-per-node=4     # Number of Cores (CPU Processors). 2 GPU nodes have 52 available CPU cores
+#SBATCH --gres=gpu:a100:2       # Extra line for gpu jobs specifying number of gpus
 #SBATCH --mem=10G               # Memory request per node
 #SBATCH --mail-user=<email_id>  # Designate email address for job communications
 #SBATCH --mail-type=END         # Events options are job BEGIN, END, NONE, FAIL, REQUEUE
@@ -12,29 +12,28 @@
 #SBATCH --error=<file_path>     # Path for errors must already exist - e.g. /path/job.err
 #SBATCH --job-name="<name>"     # Name of job
 
-# unload any modules that carried over from your command line session
-module purge
 
 # add a project directory to your PATH (if needed)
 # export PATH=$PATH:/projects/p30009/<dirname>
 
 # load modules you need to use
-module load gromacs/5.0.4-cuda
+module purge
+module use /software/spack_production/spack/share/spack/modules/linux-rhel7-x86_64/
+module load gcc/10.3.0-gcc
+module load intel-oneapi-compilers/2021.3.0-gcc
+module load intel-mkl/2020.4.304-intel
+module load cuda/11.4.0-intel
+source /software/spack_production/spack/opt/spack/linux-rhel7-x86_64/intel-2021.3.0/gromacs-2021.5/bin/GMXRC
 
-## A command you actually want to execute: Example showed for running gmx
+export OMP_NUM_THREADS=13
 
-## You may want to create the tpr for mdrun in the cluster core itself for compatibility reasons
-gmx_mpi grompp -f sim_eq.mdp -p PA.top -c sim_min.gro -o sim_eq.tpr -maxwarn 2
 
-## Run mdrun on single core. Throw the output to out.log
-# mpirun -np 1 gmx_mpi mdrun -deffnm sim_eq -v -cpt 30 &> out.log
+# run
+gmx grompp -f em.mdp -c *_water.gro -p topol.top -o em.tpr
+gmx mdrun -deffnm em -v -ntmpi 4 -ntomp 13
+gmx grompp -f md_anneal.mdp -c em.gro -p topol.top -o md.tpr
+gmx mdrun -ntmpi 4 -ntomp 13 -deffnm md -v -cpt 5 -nb gpu -pme gpu -npme 1 &> log
 
-## Run mdrun on multiple cores. Throw the output to out.log
-mpirun -np 2 gmx_mpi mdrun -deffnm sim_eq -v -cpt 30 &> out.log
-
-# Using a quick benchmark, 
-# GROMACS 2020.3 runs fastest using GPU with the following command on Quest
-# mpirun -np 4 gmx_mpi mdrun -deffnm md -v -cpt 5 -rdd 2 -nb gpu -pme gpu -npme 1 # using 4 GPUs
-# gmx mdrun -deffnm md -v -cpt 5 -nb gpu -pme gpu -update gpu # using 1 GPU
-
+# continue initial run
+#gmx mdrun -ntmpi 4 -ntomp 13 -deffnm md -v -cpt 5 -cpi md.cpt -append -nb gpu -pme gpu -npme 1 &> log
 
